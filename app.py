4bd24456e3c07d6fc4d45a906fca2c1e817373e0
jean-luc KRIKER <dahai0013@gmail.com>
2018-10-21 21:57:39 +0100

    commit 5

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from flask import Flask
 from flask import render_template, url_for, abort, jsonify, request
 app = Flask(__name__)
 
-background_scripts = {}
-
-def run_script(id):
-    subprocess.call(["/path/to/yourscript.py", "argument1", "argument2"])
-    background_scripts[id] = True
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,22 +18,12 @@ def send():
         argument1 = request.form['argument1']
         argument2 = request.form['argument2']
         command = 'python ' + script_name +' '+ ipaddress +' '+ argument1 +' '+ argument2
-        return render_template('command.html', command=command )
+
+        #
+        script_output = subprocess.call("python hello_world.py")
+
+        return render_template('index.html', script_output=script_output , command=command )
     return render_template('index.html')
-
-@app.route('/generate')
-def generate():
-    id = str(uuid.uuid4())
-    background_scripts[id] = False
-    threading.Thread(target=lambda: run_script(id)).start()
-    return render_template('processing.html', id=id)
-
-@app.route('/is_done')
-def is_done():
-    id = request.args.get('id', None)
-    if id not in background_scripts:
-        abort(404)
-    return jsonify(done=background_scripts[id])
 
 if __name__ == "__main__":
     # Only for debugging while developing
